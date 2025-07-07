31a84bbaf2a90c2250ad5027fc99e2334a49f187
LJ <levijoaquinfarias@yahoo.com.ar>
2025-07-07 11:45:18 -0300

    Digitos y mas

--- Cap8Ej1.java
+++ Cap8Ej1.java
@@ -1,7 +1,11 @@
 import matematicas.Varias;
 public class Cap8Ej1{
 public static void main(String[] args) {
-    System.out.println(Varias.esCapicua(444));
-    System.out.println(Varias.esPrimo(13));
+
+    //System.out.println(Varias.esCapicua(444));
+    //System.out.println(Varias.esPrimo(13));
+   // System.out.println(Varias.siguentePrimo(31));
+   //System.out.println(Varias.potencia(5, -5));
+   System.out.println(Varias.Digitos(409));
 }
 }
--- matematicas/Varias.java
+++ matematicas/Varias.java
@@ -36,11 +36,68 @@ public class Varias {
         if (n<2){
             return false;
         }else {
-            for (int i = n; i >2; i--){
+            for (int i = n /2; i >2; i--){
                 if (n % i ==0){
                     return false;
                 }
             }
         }return true;
     }
+    /**
+     * Devuelve el siguente numero primo del numero ingresado 
+     * @param num Numero ingresado
+     * @return
+     */
+    public static int siguentePrimo(int num){
+        //++ numero, incrementa el numero antes de enviarlo
+        // para que no devuelva el mismo numero inicial en el caso de  
+        while (!esPrimo(++num)){
+         System.out.print(num + " ");   
+        };
+        return num;
+    }
+    /**
+     * Dada una base y un exponente nos devuelve la potencia
+     * @param base
+     * @param exponente
+     * @return
+     */
+    public static long potencia (int base, int exponente){
+        //Mi el exponente es 0 siempre devolvemos 1
+        if (exponente ==0) {
+            return 1;
+        }
+        //El el caso de que exponente sea negativo llamamos a esta funcion de manera recursiva 
+        if (exponente < 0) {
+            return 1 / potencia(base, -exponente);
+        }
+        //Inicializamos n como acumulador en 1 para poder multiplicar en cada una de las interaciones del ciclo
+        int n=1;
+        for(int i =0; i < Math.abs(exponente); i++){
+            n = n * base; 
+        }
+        return n;
+
+    } 
+    /**
+     * devuelve la cantidad como un numero entero, la cantidad del digito 
+     * @param nu
+     * @return
+     */
+    public static int Digitos (int nu){
+        if(nu <0){
+            nu = -nu;
+        }
+        if (nu == 0) {
+            return 1;
+        }
+        else{ 
+           int n = 0;
+        while (nu > 0) {
+            nu = nu / 10; // esto le quita un digito a numero 
+            n++; // Incrementa la cantidad de digitos
+        }
+        return n;
+    }
+}
 }
